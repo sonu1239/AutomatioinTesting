f0a121c920b730d42877ab34d2c27911ea946d83
sonu1239 <ss316742@gmail.com>
2022-08-02 14:33:33 +0530

    add file

--- seleniumDemoProject/src/test/java/com/Demo/Facebook_Login.java
+++ seleniumDemoProject/src/test/java/com/Demo/Facebook_Login.java
@@ -2,15 +2,18 @@ package com.Demo;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class Facebook_Login {
 
 	@Test
 	public void login() throws InterruptedException {
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\MY\\Downloads\\chromedriver_win32\\chromedriver.exe");
-		 WebDriver driver = new ChromeDriver();
+		//System.setProperty("webdriver.chrome.driver", "C:\\Users\\MY\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\MY\\Downloads\\geckodriver-v0.31.0-win64\\geckodriver.exe");
+		 //WebDriver driver = new ChromeDriver();
+		 WebDriver driver = new FirefoxDriver();
 		  Thread.sleep(4000);
 	    driver.get("https://www.facebook.com/");
 	    driver.manage().window().maximize();
